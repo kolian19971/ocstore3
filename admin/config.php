1a4d6372d8f04390a891226c205030c12792da41
NT <user@users-MacBook-Pro.local>
2021-08-03 23:56:42 +0300

    Update

---
+++ admin/config.php
@@ -0,0 +1,37 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test.hable.club/admin/');
+define('HTTP_CATALOG', 'http://test.hable.club/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test.hable.club/admin/');
+define('HTTPS_CATALOG', 'http://test.hable.club/');
+
+// DIR
+define('DIR_APPLICATION', '/home/zj430243/hable.club/test/admin/');
+define('DIR_SYSTEM', '/home/zj430243/hable.club/test/system/');
+define('DIR_IMAGE', '/home/zj430243/hable.club/test/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', '/home/zj430243/hable.club/test/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'zj430243.mysql.tools');
+define('DB_USERNAME', 'zj430243_test');
+define('DB_PASSWORD', 'x3tG!^3X8u');
+define('DB_DATABASE', 'zj430243_test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
+define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');
---
+++ system/storage/cache/20/2042641c70dd3253a24f3843803b26910eb5f66e8c0649ab3e0526462f1d5e77.php
@@ -0,0 +1,52 @@
+<?php
+
+/* default/template/common/search.twig */
+class __TwigTemplate_985af7044bc2bdd2bd7427070eae6e34f570eebebb64c6948b927cc0dd16fcd8 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<div id=\"search\" class=\"input-group\">
+  <input type=\"text\" name=\"search\" value=\"";
+        // line 2
+        echo (isset($context["search"]) ? $context["search"] : null);
+        echo "\" placeholder=\"";
+        echo (isset($context["text_search"]) ? $context["text_search"] : null);
+        echo "\" class=\"form-control input-lg\" />
+  <span class=\"input-group-btn\">
+    <button type=\"button\" class=\"btn btn-default btn-lg\"><i class=\"fa fa-search\"></i></button>
+  </span>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "default/template/common/search.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  22 => 2,  19 => 1,);
+    }
+}
+/* <div id="search" class="input-group">*/
+/*   <input type="text" name="search" value="{{ search }}" placeholder="{{ text_search }}" class="form-control input-lg" />*/
+/*   <span class="input-group-btn">*/
+/*     <button type="button" class="btn btn-default btn-lg"><i class="fa fa-search"></i></button>*/
+/*   </span>*/
+/* </div>*/
